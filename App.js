b5ed5b4e4167ffd4c17c44a258250a13df13a543
Isaac Daniel Silva Uliam <isaac15102008@gmail.com>
2025-09-26 22:49:44 +0845

    2b_3b2025_prova

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>2o Ano B</Text>
+      <Text>2025</Text>
+      <Text>José Emilio Da Silva Góis</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
